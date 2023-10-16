bea420b425236b1c51dce61f2f5f8aec9b0904a9
Vladadon <v.golubin2017@yandex.ru>
2023-10-16 16:20:20 +0300

    Add some work and compleate 20.md

---
+++ Task_5.py
@@ -0,0 +1,11 @@
+class User:
+ def show(self):
+  return '+++'
+user1 = User()
+print(user1.show())
+
+class Employee:
+    def show(self):
+        return 'name = None'
+employee1 = Employee()
+print(employee1.show())
